d50a6c2f8b62c720db229b1baa3d2f49c93a3171
bangAzee22 <124791662+bangAzee22@users.noreply.github.com>
2023-03-01 20:55:31 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -7,8 +7,8 @@ let name = await conn.getName(who)
 
   
  const ini = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 🥺`, `⁨kabulsaputra@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://chat.whatsapp.com/F8JCtpCaSRFLmFf70TvF3R`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🔥 Bot WhatsApp 🐣`, `📵 Don't spam/call me 😢`, `Nothing`, `🇮🇩 Indonesia`, `🚀 https://chat.whatsapp.com/F8JCtpCaSRFLmFf70TvF3R`, `🤖 Dont Forget To Donate!`]
+    [`${nomorown}`, `${await conn.getName(nomorown+6282169638085'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Jangan call Aku 🥺`, `⁨kabulsaputra@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://chat.whatsapp.com/F8JCtpCaSRFLmFf70TvF3R`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🔥 Bot WhatsApp 🐣`, `📵 Jangn spam/call Aku,Nanti Aku Block 😢`, `Nothing`, `🇮🇩 Indonesia`, `🚀 https://chat.whatsapp.com/F8JCtpCaSRFLmFf70TvF3R`, `🤖 Dont Forget To Donate!`]
   ], fkontak)
   await conn.send2ButtonDoc(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor owner ku, jangan call/spam yah kak😌`, wm, 'Menu', '.menu', 'Donasi', '.donasi', ini, { contextInfo: { forwardingScore: fsizedoc, externalAdReply: { body: 'Tes', containsAutoReply: true, mediaType: 1, mediaUrl: hwaifu.getRandom(),  renderLargerThumbnail: true, showAdAttribution: true, sourceId: 'Tes', sourceType: 'PDF', previewType: 'PDF', sourceUrl: sgc, thumbnail: fs.readFileSync('./thumbnail.jpg'), thumbnailUrl: sgc, title: wm}}})
   }
